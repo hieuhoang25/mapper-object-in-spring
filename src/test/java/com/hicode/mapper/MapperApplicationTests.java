27a2f6a1469b94d171ea0948486dfcdb5a903138
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2024-02-20 21:55:05 +0700

    init

---
+++ src/test/java/com/hicode/mapper/MapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hicode.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
